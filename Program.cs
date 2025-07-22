c30b2a85f6ea16dfa07a2647cb149c33a158e612
simeonebenezer <simeon.ebe770@gmail.com>
2025-07-21 21:23:22 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace CoreConsole;
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+		// Console.WriteLine("Hello, World!");
+		
+		var config = new ConfigurationBuilder()
+			.AddCommandLine(args)
+			.Build();
+
+		var host = new WebHostBuilder()
+			.UseKestrel()
+			.UseConfiguration(config)
+			.UseStartup<Startup>()
+			.Build();
+
+		host.Run();
+    }
+}
+
+[Route("[controller]")]
+public class HelloWorldController : Controller
+{
+	[HttpGet]
+	public string Index()
+	{
+
+		Console.WriteLine("Hello World, Han Sti from [ACR][A515]/CoreConsole!");
+		return "Hello World, Han Sti from [ACR][A515]/CoreConsole!";
+	}
+}
+
+public class Startup
+{
+	public void ConfigureServices(IServiceCollection services)
+	{
+		services.AddMvc();
+	}
+
+	public void Configure(IApplicationBuilder app)
+	{
+		app.UseMvc();
+	}
+}
